6a1a0a524ac4719887187db240476a1d18da1d59
Rishav Adarsh <ris_adrsh@Rishavs-MacBook-Air.local>
2026-05-05 00:52:53 +0530

    Health checkup log

--- backend/src/main/java/com/cinema/cineshow/api/controller/HealthController.java
+++ backend/src/main/java/com/cinema/cineshow/api/controller/HealthController.java
@@ -1,15 +1,19 @@
 package com.cinema.cineshow.api.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+@Slf4j
 @RestController
 public class HealthController {
+    private static final String LOG_IDENTIFIER = "[HealthController]";
 
     @GetMapping("/health")
     public Map<String, String> health() {
+        log.info("{}: Health check endpoint is called !!", LOG_IDENTIFIER);
         return Map.of("status", "UP", "message", "CineShow API is running");
     }
 }
